d144e97783da491afc0d231950998a5dbe87568e
tranngoc769 <quangtn@ahamove.com>
2025-03-07 10:55:17 +0700

    revert: remove code that not in prod version

--- src/transforms/elastic.transform.ts
+++ src/transforms/elastic.transform.ts
@@ -63,7 +63,7 @@ export class ElasticTransform {
             newKey = `address_parts.${key}`
             break
           case "address":
-            if (!parsedText.street) newKey = "name.default"
+            newKey = "name.default"
             break
           default:
             return null
@@ -71,19 +71,6 @@ export class ElasticTransform {
 
         if (!value || !newKey) {
           return null
-        }
-
-        if (newKey === "parent.locality") {
-          if (value.match(/(Phường)\s\D/)) value = value.replace("Phường ", "")
-        }
-        if (newKey === "parent.county") {
-          if (value.match(/(Quận)\s\D/)) value = value.replace("Quận ", "")
-        }
-        if (
-          newKey === "address_parts.street" &&
-          parsedText?.address?.includes("Hà Nội")
-        ) {
-          if (value.match(/^(Phố)\s\D/i)) value = value.replace("Phố ", "")
         }
 
         if (newKey === "address_parts.number") {
